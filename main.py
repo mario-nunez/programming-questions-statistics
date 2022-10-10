7d0c464a934ab779788ba60c0d25fc2065c663e8
Mario <mario.nunez.callejo@gmail.com>
2022-10-10 17:46:47 +0200

    Create seaborn visualizations and beautify them

--- main.py
+++ main.py
@@ -162,50 +162,52 @@ class MainClass:
 
         # Visualise results with seaborn
 
-        sns.set(rc={"figure.figsize":(13, 9)})
-        
-        #set seaborn plotting aesthetics as default
-        
-
-        #define plotting region (2 rows, 2 columns)
-        fig, axes = plt.subplots(2, 2)
-        fig.suptitle('Stackoverflow visualizations')
-
-        #create boxplot in each subplot
+        fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(11,9))
+        fig.subplots_adjust(hspace=0.3, wspace=0.3)
+        fig.suptitle('Stackoverflow visualizations', fontsize=20)
 
         # Top 5 questions with most views
         data_most_views = self.data_parsed_df\
             .sort_values("views", ascending=False).head(5)
-        sns.barplot(
-            x="question_id", y="views", data=data_most_views, orient="v",
-            order=data_most_views.sort_values("views", ascending=False).question_id,
-            ax=axes[0,0]
-            ).set(title="Top 5 questions with most views")
+        ax1 = sns\
+            .barplot(x="question_id", y="views", data=data_most_views,
+                     orient="v", ax=axes[0,0], order=data_most_views\
+                        .sort_values("views", ascending=False).question_id)\
+            .set(title="Top 5 questions with most views",
+                 xlabel="Question ID", ylabel="Views")
+
+
+        # Top 5 questions with most answers
+        data_most_answers = self.data_parsed_df\
+            .sort_values("num_answers", ascending=False).head(5)
+        sns\
+            .barplot(x="question_id", y="num_answers", data=data_most_answers,
+                     orient="v", ax=axes[0,1], order=data_most_answers\
+                        .sort_values("num_answers", ascending=False).question_id)\
+            .set(title="Top 5 questions with most answers",
+                 xlabel="Question ID", ylabel="Number of answers")
 
         # Top 5 questions with most votes
         data_most_votes = self.data_parsed_df\
             .sort_values("votes", ascending=False).head(5)
-        sns.barplot(
-            x="question_id", y="votes", data=data_most_votes, orient="v",
-            order=data_most_votes.sort_values("votes", ascending=False).question_id,
-            ax=axes[0,1]).set(title="Top 5 questions with most votes")
+        sns\
+            .barplot(x="question_id", y="votes", data=data_most_votes,
+                     orient="v", ax=axes[1,0], order=data_most_votes\
+                        .sort_values("votes", ascending=False).question_id)\
+            .set(title="Top 5 questions with most votes",
+                 xlabel="Question ID", ylabel="Votes")
 
-        # Top 5 questions with most views
-        data_most_views = self.data_parsed_df\
-            .sort_values("views", ascending=False).head(5)
-        sns.barplot(
-            x="question_id", y="views", data=data_most_views, orient="v",
-            order=data_most_views.sort_values("views", ascending=False).question_id,
-            ax=axes[1,0]
-            ).set(title="Top 5 questions with most views")
+        # Number of answers of the top 5 questions with most votes
+        sns\
+            .barplot(x="question_id", y="num_answers", data=data_most_votes,
+                     orient="v", ax=axes[1,1], order=data_most_votes\
+                        .sort_values("votes", ascending=False).question_id)\
+            .set(title="Number of answers of the top 5 questions with most votes",
+                 xlabel="Question ID", ylabel="Number of answers")
 
-        # Top 5 questions with most votes
-        data_most_votes = self.data_parsed_df\
-            .sort_values("votes", ascending=False).head(5)
-        sns.barplot(
-            x="question_id", y="votes", data=data_most_votes, orient="v",
-            order=data_most_votes.sort_values("votes", ascending=False).question_id,
-            ax=axes[1,1]).set(title="Top 5 questions with most votes")
+        fig2, axes = plt.subplots(nrows=2, ncols=2, figsize=(11,9))
+        fig2.subplots_adjust(hspace=0.3, wspace=0.3)
+        fig2.suptitle('Other visualizations', fontsize=20)
         
         plt.show()
 
